fe86f80b284c75cb2e662d8f9138329961cac330
Jack Zhang <jack.zhang@aspiraconnect.com>
2020-09-25 14:05:13 +0800

    ch16

--- ch10-books/books/models.py
+++ ch10-books/books/models.py
@@ -44,3 +44,14 @@ class Review(models.Model): # new
 
     def __str__(self):
         return self.review
+
+
+class BookTest(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
